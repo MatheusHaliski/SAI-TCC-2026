84dadff873530f5715e7ed2e794ad0b21e4fda12
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2026-03-06 18:31:13 -0300

    Update AuthShell.tsx

--- app/components/AuthShell.tsx
+++ app/components/AuthShell.tsx
@@ -41,7 +41,7 @@ export default function AuthShell({
         >
           <div className="flex h-full w-full items-center justify-center">
             <img
-              src="/VI.jpeg"
+              src="/PUCPR.jpeg"
               alt="Velion"
               className="h-full w-full object-contain"
             />
