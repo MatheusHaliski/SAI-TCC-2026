c8089af39dbf2ba55597f2eeb5e881f02de6011d
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2026-03-31 23:09:46 -0300

    Update ContextSectionMenu.tsx

--- app/components/navigation/ContextSectionMenu.tsx
+++ app/components/navigation/ContextSectionMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import ContextSectionItem from './ContextSectionItem';
 
 interface ContextSectionMenuProps {
